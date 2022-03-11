3532973d8455c05fb2f20da9b60f628f30e96e11
Airi-hub <dreamair25@gmail.com>
2022-03-11 11:19:20 +0900

    ログインページの開 発設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from "vue-router"
+import Home from "../views/Home.vue"
+import Login from "../views/Login.vue"
 
 const routes = [
   {
@@ -16,11 +17,16 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
-];
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-});
+})
 
-export default router;
+export default router
